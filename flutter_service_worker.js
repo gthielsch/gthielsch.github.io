6991ea5ad90ce29b957e9f0f1d6bb812d54bb2ec
Gabriel Thielsch <gthielsch@gmail.com>
2023-02-07 08:38:22 +0100

    version 0.0.2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,27 +3,28 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "b9ed605abb4d4ad4b61e6d0b73687484",
-"index.html": "e03c6563fdaeb2fde0a6bc8c8e40b784",
-"/": "e03c6563fdaeb2fde0a6bc8c8e40b784",
-"main.dart.js": "3a654f094e7a2e7f5da14fb551b97462",
-"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
+  "version.json": "2adb028f57d7476da9d9234e8e7b8109",
+"index.html": "4ef55b930bfcb0bff3da95eebba05531",
+"/": "4ef55b930bfcb0bff3da95eebba05531",
+"main.dart.js": "2e95e90b9ebe0b1b9812139ef674d262",
+"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "7e0c6646dbbbc5591a06d0f3005f5faf",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "5e6ac536fbec58719a633ba7af35bb6f",
+"assets/AssetManifest.json": "ab958083fd628c19f4e363f095392586",
+"assets/NOTICES": "d4a01c25a4ed0986e2d259cf2f001c07",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/shaders/ink_sparkle.frag": "9fcf5fa75e4d0447e9a45aa9776b9aa6",
-"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
-"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
-"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
-"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62"
+"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
+"assets/assets/cardinal_symptoms_v0.3_mini.json": "5d2ffa42edd6b81e0568129772c42481",
+"assets/assets/cardinal_symptoms_v0.3.json": "c0fd8ad9d0c3dd4637e17ecdc6d563f7",
+"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
+"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
+"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
 };
 
 // The application shell files that are downloaded before a service worker can
